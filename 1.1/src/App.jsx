d2db60ee76625e9b7496998bba8faa9158adca44
unknown <brunobossomartins1@gmail.com>
2024-12-23 21:36:59 -0300

    first commit

---
+++ 1.1/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Teste = () => {
+  return (
+    <div>
+      <h1>Ola</h1>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <a href="https://origamid.com">Origamid</a>;
+      <Teste />;
+    </div>
+  );
+};
+
+export default App;
