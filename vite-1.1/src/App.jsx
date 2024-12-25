4d76e3484a87a9b542c0fa45cde8cbb603f5355b
unknown <brunobossomartins1@gmail.com>
2024-12-24 23:23:22 -0300

    funcoes basicas do jxs

--- vite-1.1/src/App.jsx
+++ vite-1.1/src/App.jsx
@@ -1,5 +1,27 @@
+const titulo = <h1>Esse é um titulo</h1>;
+
 const App = () => {
-  return <div>App React</div>;
+  const nome = 'Bruno';
+  const ativo = 0;
+
+  const carro = {
+    marca: 'Honda',
+    rodas: '4',
+  };
+
+  const estiloB = {
+    color: 'green',
+    fontSize: '2rem',
+  };
+  return (
+    <>
+      {titulo}
+      <p style={estiloB}>{new Date().getFullYear()}</p>
+      <p>{carro.marca}</p>
+      <p>{carro.rodas}</p>
+      <p className={ativo ? 'ativo' : 'inativo'}>{nome + ' 25 anos'}</p>
+    </>
+  );
 };
 
 export default App;
