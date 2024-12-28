21ebd490f1178fcd51114db4b242a470d6c2782b
Bruno Martins <brunobossomartins1@gmail.com>
2024-12-28 01:44:26 -0300

    ajustado arquivo app.jsx

--- vite-1.1/src/exercicio2.jsx
+++ vite-1.1/src/exercicio2.jsx
@@ -1,34 +1,50 @@
-// const App = () => {
-//   const produtos = ['notebook ', 'tablet ', 'iphone'];
-//   return (
-//     <>
-//       <ul>
-//         {produtos.map((produto) => (
-//           <li key={produto}>{produto}</li>
-//         ))}
-//       </ul>
-//     </>
-//   );
-// };
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
 
 const App = () => {
-  const livros = [
-    { nome: 'A Agua de Bruno', ano: 1995 },
-    { nome: 'A Torneira Feia do Hotel', ano: 2000 },
-    { nome: 'The Devil with Black Eyes', ano: 2015 },
-  ];
   return (
-    <>
-      <ul>
-        {livros
-          .filter((livro) => livro.ano >= 2000)
-          .map((livro) => (
-            <li key={livro.nome}>
-              {livro.nome}, {livro.ano}
-            </li>
-          ))}
-      </ul>
-    </>
+    <section>
+      {produtos
+        .filter((produto) => Number(produto.preco.replace('R$ ', '')) > 1500)
+        .map((produto) => (
+          <div key={produto.id}>
+            <h1>{produto.nome}</h1>
+            <p>Preço: {produto.preco}</p>
+            <ul>
+              {produto.cores.map((cor, index) => (
+                <li
+                  key={index}
+                  style={{
+                    color: 'white',
+                    backgroundColor: cor,
+                    listStyle: 'none',
+                  }}
+                >
+                  {cor}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+    </section>
   );
 };
 
