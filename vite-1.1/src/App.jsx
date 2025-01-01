f328ff5be9cc19cc9479ccba2f78507c24d63e56
Bruno Martins <brunobossomartins1@gmail.com>
2024-12-31 23:40:54 -0300

    adicionado estudo de propriedades em jsx

--- vite-1.1/src/App.jsx
+++ vite-1.1/src/App.jsx
@@ -1,38 +1,29 @@
+// const Titulo = ({ cor, texto, children }) => {
+//   return (
+//     <h1 style={{ color: cor }}>
+//       {texto}, {children}
+//     </h1>
+//   );
+// };
+
+import Propriedades from './Propriedades/Form';
+
 // const App = () => {
-//   const produtos = ['notebook ', 'tablet ', 'iphone'];
 //   return (
 //     <>
-//       <ul>
-//         {produtos.map((produto) => (
-//           <li key={produto}>{produto}</li>
-//         ))}
-//       </ul>
+//       <Titulo cor="red" texto="Meu titulo 1">
+//         Isso é o children
+//         <p>Teste</p>
+//       </Titulo>
+//       <Titulo cor="blue" texto="Meu titulo 2" />
 //     </>
 //   );
 // };
 
-import Header from './Header.jsx';
-import Footer from './Footer.jsx';
-import Form from './Form/Form.jsx';
-
-const Teste = () => {
-  const active = true;
-  if (active) {
-    return <p>Teste</p>;
-  } else {
-    return null;
-  }
-};
-
 const App = () => {
   return (
     <>
-      <div>
-        <Teste />
-        <Header />
-        <Form />
-        <Footer />
-      </div>
+      <Propriedades />
     </>
   );
 };
