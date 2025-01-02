e99d7cdcb3404f78553171c3f1cd1071a491bdb9
Bruno Martins <brunobossomartins1@gmail.com>
2025-01-02 20:47:55 -0300

    adicionado arquivo de desafio e propriedades

---
+++ vite-1.1/src/Propriedade.jsx
@@ -0,0 +1,43 @@
+// const Titulo = ({ cor, texto, children }) => {
+//   return (
+//     <h1 style={{ color: cor }}>
+//       {texto}, {children}
+//     </h1>
+//   );
+// };
+
+import Propriedades from './Propriedades/Form';
+
+// const App = () => {
+//   return (
+//     <>
+//       <Titulo cor="red" texto="Meu titulo 1">
+//         Isso é o children
+//         <p>Teste</p>
+//       </Titulo>
+//       <Titulo cor="blue" texto="Meu titulo 2" />
+//     </>
+//   );
+// };
+
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+import Form from './Form/Form.jsx';
+
+const Teste = () => {
+  const active = true;
+  if (active) {
+    return <p>Teste</p>;
+  } else {
+    return null;
+  }
+};
+
+const App = () => {
+  return (
+    <>
+      <Propriedades />
+    </>
+  );
+};
+export default App;
