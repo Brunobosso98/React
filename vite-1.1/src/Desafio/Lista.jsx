25a1c2777ec5510cc6e80f29508faabca136b463
Bruno Martins <brunobossomartins1@gmail.com>
2025-01-02 20:58:42 -0300

    feito primeira parte do desafio

---
+++ vite-1.1/src/Desafio/Lista.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Lista = () => {
+  return (
+    <ul>
+      <li>
+        <a href="../App.jsx">Home</a>
+      </li>
+      <li>
+        <a href="./">Produtos</a>
+      </li>
+    </ul>
+  );
+};
+
+export default Lista;
