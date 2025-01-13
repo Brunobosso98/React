c79f68c8d4fd9ad504ac2685724d64c692a71ef7
Bruno Martins <brunobossomartins1@gmail.com>
2025-01-13 15:57:52 -0300

    adicionado uso de hooks customizados

--- vite-1.1/src/App.jsx
+++ vite-1.1/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import useLocalStorage from './useLocalStorage.jsx';
 
 const App = () => {
-  return <></>;
+  const [produto, setProduto] = useLocalStorage('produto', '');
+
+  function handleClick({ target }) {
+    setProduto(target.innerText);
+  }
+  return (
+    <>
+      <p>Produto preferido: {produto}</p>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+    </>
+  );
 };
 
 export default App;
