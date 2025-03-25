49537e0b872ee788f014a5313cbb6a951c0725ab
Bruno Martins <brunobossomartins1@gmail.com>
2025-03-24 22:33:51 -0300

    componente input

--- vite-1.1/src/App.jsx
+++ vite-1.1/src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const coresArray = ['azul', 'vermelho', 'verde', 'amarelo', 'laranja', 'cinza'];
-
 const App = () => {
-  return <></>;
+  return <form></form>;
 };
 
 export default App;
