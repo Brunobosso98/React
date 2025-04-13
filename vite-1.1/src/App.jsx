ed2a6f4e4dd665f01249163cb93a6334bd095a4e
Bruno Martins <brunobossomartins1@gmail.com>
2025-04-12 21:52:29 -0300

    componente checkbox

--- vite-1.1/src/App.jsx
+++ vite-1.1/src/App.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import Checkbox from './Hooks/Checkbox/Checkbox.jsx';
 
 const App = () => {
-  return <form></form>;
+  const [linguagens, setLinguagens] = React.useState([]);
+  const [termos, setTermos] = React.useState([]);
+  if (termos.length > 0) {
+    console.log('enviar');
+  }
+
+  return (
+    <form>
+      <h2>Termos</h2>
+      <Checkbox
+        options={['Li e aceito os termos']}
+        value={termos}
+        setValue={setTermos}
+      />
+      <h2>Checkbox</h2>
+      <Checkbox
+        options={['JavaScript', 'Python', 'Ruby']}
+        value={linguagens}
+        setValue={setLinguagens}
+      />
+    </form>
+  );
 };
 
 export default App;
